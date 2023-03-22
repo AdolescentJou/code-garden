82d185ebdc52f4afec637d89e77a95b78db1b2e7
jasperou <jasperou@tencent.com>
2023-03-22 11:50:38 +0800

    feat:新增demo

--- package-util/节流.ts
+++ package-util/节流.ts
@@ -4,7 +4,7 @@
  * @param {number} wait
  * @returns {Function}
  */
- export function throttleWithStartAndEnd(fn: Function, wait: number): Function {
+export function throttleWithStartAndEnd(fn: Function, wait: number): Function {
   let timer: any = null;
   let startTime = Date.now();
   return function (this: any, ...args) {
@@ -21,5 +21,45 @@
         startTime = Date.now();
       }, remainTime);
     }
+  };
+}
+
+// 节流，普通版
+function throttle(handler, wait) {
+  wait = wait || 300;
+  var lastTime = 0;
+  return function () {
+    var _self = this,
+      _args = arguments;
+    var nowTime = new Date().getTime();
+    if (nowTime - lastTime > wait) {
+      handler.apply(_self, _args);
+      lastTime = nowTime;
+    }
+  };
+}
+
+// 节流，复杂版（定时器版）
+function throttleW(fn, interval, context, firstTime) {
+  let timer;
+  firstTime = typeof firstTime !== 'undefined' ? firstTime : true;
+  return function () {
+    let args = arguments;
+    let __me = this;
+    if (typeof context !== 'undefined') {
+      __me = context;
+    }
+    if (firstTime) {
+      fn.apply(__me, args);
+      return (firstTime = false);
+    }
+    if (timer) {
+      return false;
+    }
+    timer = setTimeout(function () {
+      clearTimeout(timer);
+      timer = null;
+      fn.apply(__me, args);
+    }, interval);
   };
 }
